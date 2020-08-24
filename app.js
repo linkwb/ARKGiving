00b7ae5c4d7096ddd79c9a210a2778184a5aa823
wbryden <linkwsb@gmail.com>
2020-08-24 00:32:39 -0400

    Fix callback urls for google and facebook to work with heroku.

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/arkgiving",
+    callbackURL: "http://arkgiving.herokuapp.com/auth/google/arkgiving",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -108,7 +108,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FB_APP_ID,
     clientSecret: process.env.FB_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/arkgiving"
+    callbackURL: "http://arkgiving.herokuapp.com/auth/facebook/arkgiving"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);

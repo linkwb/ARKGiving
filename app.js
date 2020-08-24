1fe01af24e80e4992993ad838b2e903dd58d6824
wbryden <linkwsb@gmail.com>
2020-08-24 00:39:45 -0400

    Fix callback urls for google and facebook to https.

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://arkgiving.herokuapp.com/auth/google/arkgiving",
+    callbackURL: "https://arkgiving.herokuapp.com/auth/google/arkgiving",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -108,7 +108,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FB_APP_ID,
     clientSecret: process.env.FB_APP_SECRET,
-    callbackURL: "http://arkgiving.herokuapp.com/auth/facebook/arkgiving"
+    callbackURL: "https://arkgiving.herokuapp.com/auth/facebook/arkgiving"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);

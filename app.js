e52d9d6553cabc4d96deacc4612bf3b63718d6c9
wbryden <linkwsb@gmail.com>
2024-01-02 12:46:34 -0500

    Updated package requirements
    Updated requirements to modernize app and moved away from Heroku since it deprecated free hosting.

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.connect("mongodb+srv://admin-will:fMswrK9eHKaxtCub@requests.iaqjw.mongo
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-mongoose.set('useCreateIndex', true);
+//mongoose.set('useCreateIndex', true);
+mongoose.set('strictQuery', true);
 
 /* Request & User Schemas
  ***********************************/
@@ -91,8 +92,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    //callbackURL: "http://localhost:3000/auth/google/arkgiving",
-    callbackURL: "https://arkgiving.herokuapp.com/auth/google/arkgiving",
+    callbackURL: "http://localhost:3000/auth/google/arkgiving",
+    //callbackURL: "https://arkgiving.herokuapp.com/auth/google/arkgiving",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -109,8 +110,8 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FB_APP_ID,
     clientSecret: process.env.FB_APP_SECRET,
-    //callbackURL: "http://localhost:3000/auth/facebook/arkgiving",
-    callbackURL: "https://arkgiving.herokuapp.com/auth/facebook/arkgiving"
+    callbackURL: "http://localhost:3000/auth/facebook/arkgiving",
+    //callbackURL: "https://arkgiving.herokuapp.com/auth/facebook/arkgiving"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
